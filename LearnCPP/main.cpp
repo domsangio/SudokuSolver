b93b3dce62ecf9f85a7c3890b653684a880a1e48
domsangio <domenic_sangiovanni@yahoo.com>
2020-02-21 23:27:38 -0500

    Initial Commit

---
+++ LearnCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by Domenic SanGiovanni on 2/21/20.
+//  Copyright © 2020 Domenic SanGiovanni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
